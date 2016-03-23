8ddc00a35e71c20d944469805e043ffe463fae72
Sigfried Gold <sigfried@sigfried.org>
2016-03-23 09:10:12 -0400

    trying for heroku again

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 "use strict";
 var NO_DB = true;
 var fs = require('fs');
-var webpack = require('webpack');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-var config = require('./webpack.config');
 var compression = require('compression');
 var express = require('express');
 //var munge = require('./data/dqcdm_munge');
@@ -19,9 +15,16 @@ app.use(express.static('static/data/cms-synpuf'))
 app.use(express.static('static'))
 //app.use('/data', express.static('static/data'));
 
-var compiler = webpack(config);
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-app.use(webpackHotMiddleware(compiler));
+var isDevelopment = (process.env.NODE_ENV !== 'production');
+if (isDevelopment) {
+  var webpack = require('webpack');
+  var webpackDevMiddleware = require('webpack-dev-middleware');
+  var webpackHotMiddleware = require('webpack-hot-middleware');
+  var config = require('./webpack.config');
+  var compiler = webpack(config);
+  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+  app.use(webpackHotMiddleware(compiler));
+}
 
 app.listen(port, function(error) {
   if (error) {
